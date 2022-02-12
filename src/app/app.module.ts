53d3b695e83f0cf4b040a29b5bdcdd43454de2ad
Lemmy Mwaura <lemminton@gmail.com>
2022-02-12 12:43:40 +0300

    added default components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { UserComponent } from './components/user/user.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    UserComponent,
+    RepositoriesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
