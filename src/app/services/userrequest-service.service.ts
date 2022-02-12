6a7b94c76505aac52503ce62e6479c03c4cb142f
Lemmy Mwaura <lemminton@gmail.com>
2022-02-12 14:36:05 +0300

    Added http get request which uses promises

--- src/app/services/userrequest-service.service.ts
+++ src/app/services/userrequest-service.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserrequestService {
+  user!: 'LemmyMwaura';
 
-  constructor(private http:HttpClient) { 
-    
-  }
+  constructor(private http: HttpClient) {}
 
-  userRequest(){
-    
+  userRequest() {
+    interface Apiresponse {
+      user: string;
+      repose:string;
+    }
+
+    let promise = new Promise((resolve, reject) => {
+      this.http
+        .get(`https://api.github.com/users/${this.user}/repos?${environment.TOKEN}="+apikey`)
+        .toPromise()
+        .then(
+          (response) => {
+            resolve(response);
+          },
+          (error) => {
+            reject(error);
+          }
+        )
+    });
+
   }
 }
