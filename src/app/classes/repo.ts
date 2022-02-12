f95017c764a9cc1a1bc5a08de66b0d89b1fea8ce
Lemmy Mwaura <lemminton@gmail.com>
2022-02-12 23:11:39 +0300

    repo class refactored to allow more entries

--- src/app/classes/repo.ts
+++ src/app/classes/repo.ts
@@ -1,2 +1,14 @@
 export class Repo {
+  constructor(
+    public name: string,
+    public description: string,
+    public repo_url: string,
+    public openissues: number,
+    public forks: number,
+    public watchers: number,
+    public stars: number,
+    public visibility: number,
+    public created_at: number,
+    public mainlang: string
+  ) {}
 }
