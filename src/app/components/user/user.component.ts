5e991487d41035570077d2ddb82d99dc1bad84da
Lemmy Mwaura <lemminton@gmail.com>
2022-02-13 17:38:30 +0300

    Added user data to template and also improved the styling

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserrequestService } from 'src/app/services/userrequest-service.service';
+import { User } from 'src/app/classes/user';
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faBuilding, faMapPin, faEnvelope, faUser, faLink } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-user',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  user!:User
+  twitterIcon = faTwitter
+  githubIcon = faGithub
+  locationIcon = faMapPin
+  companyIcon = faBuilding
+  emailIcon = faEnvelope
+  userIcon = faUser
+  linkIcon = faLink
+  iconcolor = '#64ffda'
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private requestService:UserrequestService) {
   }
 
+  ngOnInit(): void {
+    this.requestService.userRequest()
+    this.user = this.requestService.user
+    console.log(this.user)
+  }
 }
