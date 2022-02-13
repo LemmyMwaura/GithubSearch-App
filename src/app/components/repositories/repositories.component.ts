131d554be6941e25591cfbd3d895a808756725bd
Lemmy Mwaura <lemminton@gmail.com>
2022-02-13 12:50:38 +0300

    Refactored repo component and added styling

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RepoService } from 'src/app/services/repo.service';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-repositories',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+  repos:any
+  githubIcon = faGithub
+  forkIcon = faCodeBranch
+  starIcon = faStar
 
-  constructor() { }
+  constructor(private repoService:RepoService ) { }
 
-  ngOnInit(): void {
+  repoLink(link:string){
+    window.open(link)
   }
 
+  liveLink(link:string){
+    window.open(link)
+  }
+
+  ngOnInit(): void {
+    this.repoService.repoRequest()
+    this.repos = this.repoService.repoArr
+  }
 }
