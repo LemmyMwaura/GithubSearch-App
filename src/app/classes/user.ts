0c5ab46e972fb8bc25928558917de25dba8a1640
Lemmy Mwaura <lemminton@gmail.com>
2022-02-13 17:36:48 +0300

    Refactored class and user service to collect more data

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,5 +1,17 @@
 export class User {
-    constructor(public name:string){
+    constructor(
+        public image:any,
+        public name:string,
+        public bio:string,
+        public followers:number,
+        public following:number,
+        public company:string,
+        public blog:string,
+        public twitter:string,
+        public email:string,
+        public accountlink:string,
+        public location:string,
+    ){
 
     }
 }
--- src/app/services/userrequest-service.service.ts
+++ src/app/services/userrequest-service.service.ts
@@ -1,34 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { User } from '../classes/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserrequestService {
-  user!: 'LemmyMwaura';
+  URL ='https://api.github.com/users/'
+  searchName = 'LemmyMwaura'
+  user!: User;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http:HttpClient) {
+    this.user = new User("", "","", 0, 0,"","","","","","")
+  }
 
   userRequest() {
-    interface Apiresponse {
-      user: string;
-      repose:string;
-    }
-
     let promise = new Promise((resolve, reject) => {
       this.http
-        .get(`https://api.github.com/users/${this.user}/repos?${environment.TOKEN}="+apikey`)
+        .get<any>(`${this.URL}${this.searchName}`)
         .toPromise()
         .then(
           (response) => {
+            response = response!
+            this.user = new User(
+              this.user.image = response.avatar_url,
+              this.user.name = response.name,
+              this.user.bio = response.bio,
+              this.user.followers = response.followers,
+              this.user.following = response.following,
+              this.user.company = response.company,
+              this.user.blog = response.blog,
+              this.user.twitter = response.twitter_username,
+              this.user.email = response.email,
+              this.user.accountlink = response.html_url,
+              this.user.location = response.location,
+            )
             resolve(response);
           },
           (error) => {
+            console.log('error')
             reject(error);
           }
         )
     });
-
+    return promise
   }
 }
