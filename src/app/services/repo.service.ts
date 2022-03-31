8cdc8f0be7fb20de17b8d3d993bfed0446787c71
Lemmy Mwaura <lemminton@gmail.com>
2022-03-31 18:09:24 +0300

    replaced depracated toPromise method with first&lastValueFrom rxjs methods

--- src/app/services/repo.service.ts
+++ src/app/services/repo.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Repo } from '../classes/repo';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -11,40 +13,41 @@ export class RepoService {
   repo!: Repo;
   repoArr: any[] = [];
 
-  constructor(private http: HttpClient, private router:Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-  repoRequest(searchName:string) {
-    if (searchName == null || undefined) return
-    this.repoArr = []
+  repoRequest(searchName: string) {
+    if (searchName == null || undefined) return;
+    this.repoArr = [];
     let promise = new Promise((resolve, reject) => {
-      this.http
-        .get<any>(`${this.URL}${searchName}/repos?access_token'=${environment.TOKEN}`)
-        .toPromise()
-        .then(
-          (response) => {
-            response!.forEach((res: any) => {
-              this.repo = new Repo(
-                res.name,
-                res.description,
-                res.html_url,
-                res.open_issues_count,
-                res.forks_count,
-                res.watchers_count,
-                res.stargazers_count,
-                res.visibility,
-                res.created_at,
-                res.language,
-                res.homepage,
-              );
-              this.repoArr.push(this.repo);
-            });
-            resolve(response);
-          },
-          (error) => {
-            error.status = 404 ? this.router.navigate(["/**"]) : console.error(error);
-            reject(error);
-          }
-        );
+      firstValueFrom(
+        this.http.get<any>(
+          `${this.URL}${searchName}/repos?access_token'=${environment.TOKEN}`
+        )
+      ).then(
+        (response) => {
+          response!.forEach((res: any) => {
+            this.repo = new Repo(
+              res.name,
+              res.description,
+              res.html_url,
+              res.open_issues_count,
+              res.forks_count,
+              res.watchers_count,
+              res.stargazers_count,
+              res.visibility,
+              res.created_at,
+              res.language,
+              res.homepage
+            );
+            this.repoArr.push(this.repo);
+          });
+          resolve(response);
+        },
+        (error) => {
+          error.status = 404 ? this.router.navigate(['/**']): console.error(error);
+          reject(error);
+        }
+      );
     });
     return promise;
   }
--- src/app/services/userrequest-service.service.ts
+++ src/app/services/userrequest-service.service.ts
@@ -3,47 +3,49 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { User } from '../classes/user';
 import { Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class UserrequestService {
-  URL =`https://api.github.com/users/`
+  URL = `https://api.github.com/users/`;
   user!: User;
 
-  constructor(private http:HttpClient, private router:Router) {
-    this.user = new User("", "","", 0, 0,"","","","","","");
+  constructor(private http: HttpClient, private router: Router) {
+    this.user = new User('', '', '', 0, 0, '', '', '', '', '', '');
   }
 
-  userRequest(searchName:string) {
-    if (searchName == null || undefined) return
+  userRequest(searchName: string) {
+    if (searchName == null || undefined) return;
     let promise = new Promise((resolve, reject) => {
-      this.http 
-        .get<any>(`${this.URL}${searchName}?access_token'=${environment.TOKEN}`)
-        .toPromise()
-        .then(
-          (response) => {
-            response = response!
-            this.user = new User(
-              this.user.image = response.avatar_url,
-              this.user.name = response.name,
-              this.user.bio = response.bio,
-              this.user.followers = response.followers,
-              this.user.following = response.following,
-              this.user.company = response.company,
-              this.user.blog = response.blog,
-              this.user.twitter = response.twitter_username,
-              this.user.email = response.email,
-              this.user.accountlink = response.html_url,
-              this.user.location = response.location,
-            )
-            resolve(response);
-          },
-          (error) => {
-            error.status = 404 ? this.router.navigate(["/**"]) : console.error(error);
-            reject(error);
-          }
+      firstValueFrom(
+        this.http.get<any>(
+          `${this.URL}${searchName}?access_token'=${environment.TOKEN}`
         )
+      ).then(
+        (response) => {
+          response = response!;
+          this.user = new User(
+            (this.user.image = response.avatar_url),
+            (this.user.name = response.name),
+            (this.user.bio = response.bio),
+            (this.user.followers = response.followers),
+            (this.user.following = response.following),
+            (this.user.company = response.company),
+            (this.user.blog = response.blog),
+            (this.user.twitter = response.twitter_username),
+            (this.user.email = response.email),
+            (this.user.accountlink = response.html_url),
+            (this.user.location = response.location)
+          );
+          resolve(response);
+        },
+        (error) => {
+          error.status = 404 ? this.router.navigate(['/**']) : console.error(error);
+          reject(error);
+        }
+      );
     });
-    return promise
+    return promise;
   }
 }
